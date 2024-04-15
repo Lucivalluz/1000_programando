1cd20466b35fed92aed1a02abfbe2e4e1f61a545
lucivalluz <lucivalsouza@hotmail.com>
2024-04-15 20:24:17 -0300

    Mario Bross

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
-console.log("Funcionou")
+const form = document.querySelector(".formulario-fale-conosco")
+const mascara = document.querySelector(".mascara-formulario")
+
+function CliqueiNoBotao()  {
+
+    form.style.left = "50%"
+    form.style.transform = "trasnlatex (-50%)"
+    mascara.style.visibility = "visible"
+}
